daf432f76201d2d0b3201c0fc1f854c05de9ecb0
jensg <jensg@b8457f37-d9ea-0310-8a92-e5e31aec5664>
2010-12-27 18:25:26 +0000

    git-svn-id: https://plugins.svn.wordpress.org/ultimate-blogroll/trunk@326416 b8457f37-d9ea-0310-8a92-e5e31aec5664

--- ultimate-blogroll.php
+++ ultimate-blogroll.php
@@ -3,7 +3,7 @@
 Plugin Name: Ultimate Blogroll
 Plugin URI: http://www.gheerardyn.be/wordpress/ultimate-blogroll
 Description: Ultimate Blogroll is a plugin which enables your visitors to submit a linktrade. Your visitors can add their own website and you can keep track of the in- and outlinks. 
-Version: 1.6.4
+Version: 1.6.5
 Author: Jens Gheerardyn
 Author URI: http://www.gheerardyn.be
 */
