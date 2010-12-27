d44547a8d63774faf0f2bd940db305e15842cc0a
jensg <jensg@b8457f37-d9ea-0310-8a92-e5e31aec5664>
2010-12-27 20:08:30 +0000

    taking plugin offline
    git-svn-id: https://plugins.svn.wordpress.org/ultimate-blogroll/trunk@326448 b8457f37-d9ea-0310-8a92-e5e31aec5664

--- config.php
+++
@@ -1,4 +0,0 @@
-<?php
-$ultimate_blogroll_db_version = "1";
-$path = ABSPATH."wp-content/plugins/ultimate-blogroll/";
-?>
--- gui/linkpartner/wizard.php
+++
@@ -1,9 +0,0 @@
-<?php
-global $gui, $path;
-$gui["title"] = "Wizard - Step 1/3";
-require_once($path."gui/header.php");
-?>
-//wizard
-<?php
-require_once($path."gui/footer.php");
-?>
