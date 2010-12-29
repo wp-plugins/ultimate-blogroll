ff53097bd2a2c04c23a5eec448e0c744d3d578f2
jensg <jensg@b8457f37-d9ea-0310-8a92-e5e31aec5664>
2010-12-29 18:38:30 +0000

    git-svn-id: https://plugins.svn.wordpress.org/ultimate-blogroll/trunk@326992 b8457f37-d9ea-0310-8a92-e5e31aec5664

---
+++ gui/linkpartner/wizard.php
@@ -0,0 +1,9 @@
+<?php
+global $gui, $path;
+$gui["title"] = "Wizard - Step 1/3";
+require_once(ABSPATH."wp-content/plugins/ultimate-blogroll/gui/header.php");
+?>
+//wizard
+<?php
+require_once(ABSPATH."wp-content/plugins/ultimate-blogroll/gui/footer.php");
+?>
