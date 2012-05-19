68f62e9f650858722e843eb3eece249d30bd0440
jensg <jensg@b8457f37-d9ea-0310-8a92-e5e31aec5664>
2012-05-19 15:27:36 +0000

    trunk reset attempt 4
    git-svn-id: https://plugins.svn.wordpress.org/ultimate-blogroll/trunk@546220 b8457f37-d9ea-0310-8a92-e5e31aec5664

--- gui/linkpartner/wizard.php
+++
@@ -1,9 +0,0 @@
-<?php
-global $gui, $path;
-$gui["title"] = "Wizard - Step 1/3";
-require_once(ABSPATH."wp-content/plugins/ultimate-blogroll/gui/header.php");
-?>
-//wizard
-<?php
-require_once(ABSPATH."wp-content/plugins/ultimate-blogroll/gui/footer.php");
-?>
